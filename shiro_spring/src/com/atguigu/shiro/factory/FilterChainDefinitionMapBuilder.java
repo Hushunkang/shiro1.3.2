beab34b7912879de3bacb69edb4f10a18a1c7cd3
Hushunkang <2752962035@qq.com>
2020-03-28 13:52:09 +0800

    Shiro 从数据库表中初始化资源和权限

--- shiro_spring/src/com/atguigu/shiro/factory/FilterChainDefinitionMapBuilder.java
+++ shiro_spring/src/com/atguigu/shiro/factory/FilterChainDefinitionMapBuilder.java
@@ -1,19 +1,22 @@
 package com.atguigu.shiro.factory;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class FilterChainDefinitionMapBuilder {
 
-//	public Map<String, String> buildFilterChainDefinitionMap(){
-//		Map<String, String> map = new LinkedHashMap<>();
-//
-//		map.put("/login.jsp", "anon");
-//		map.put("/shiro/login", "anon");
-//		map.put("/shiro/logout", "logout");
-//		map.put("/user.jsp", "authc,roles[user]");
-//		map.put("/admin.jsp", "authc,roles[admin]");
-//		map.put("/list.jsp", "admin");
-//		map.put("/**", "authc");
-//
-//		return map;
-//	}
+	public Map<String, String> buildFilterChainDefinitionMap(){
+		Map<String, String> map = new LinkedHashMap<>();
+
+		map.put("/login.jsp", "anon");
+		map.put("/shiro/login", "anon");
+		map.put("/shiro/logout", "logout");
+		map.put("/admin.jsp", "authc,roles[admin]");
+        map.put("/user.jsp", "authc,roles[user]");
+//        map.put("/list.jsp", "admin");
+		map.put("/**", "authc");
+
+		return map;
+	}
 	
 }
